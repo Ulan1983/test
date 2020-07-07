ec0e28ee18ac4aa4a02bea67bff5ca0a648adf28
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-07-07 20:06:30 +0600

    Добавил страницы toolbar и userMenu

---
+++ frontend/src/components/UI/Toolbar/AppToolbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import UserMenu from "../UserMenu/UserMenu";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+import {toggleDrawer} from "../../../store/actions/toggleDrawerActions";
+
+const useStyles = makeStyles(theme => ({
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none',
+		},
+	},
+	title: {
+		flexGrow: 1,
+	},
+	mainLink: {
+		color: 'inherit',
+		textDecoration: 'none',
+		'&:hover': {
+			color: 'inherit'
+		}
+	}
+}));
+
+
+const AppToolbar = () => {
+	const user = useSelector(state => state.users.user);
+	const dispatch = useDispatch();
+
+	const classes = useStyles();
+
+	return (
+		<>
+			{user && <AppBar position="fixed" className={classes.appBar}>
+				<Toolbar>
+					<IconButton edge="start" className={classes.menuButton} color="inherit" onClick={() => dispatch(toggleDrawer())}>
+						<MenuIcon/>
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						<Link to="/" className={classes.mainLink}>News</Link>
+					</Typography>
+					<UserMenu/>
+				</Toolbar>
+			</AppBar>}
+			<Toolbar/>
+		</>
+	);
+};
+
+export default AppToolbar;
---
+++ frontend/src/components/UI/UserMenu/UserMenu.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import {useDispatch, useSelector} from "react-redux";
+
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import Avatar from "@material-ui/core/Avatar";
+
+import {logoutUser} from "../../../store/actions/usersActions";
+
+
+
+const UserMenu = () => {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const open = Boolean(anchorEl);
+
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const dispatch = useDispatch();
+	const user = useSelector(state => state.users.user);
+
+	const logout = async () => {
+		await dispatch(logoutUser());
+	};
+
+	return (
+		<>
+			<IconButton
+				aria-label="account of current user"
+				aria-controls="menu-appbar"
+				aria-haspopup="true"
+				onClick={handleMenu}
+				color="inherit"
+				id='user'
+			>
+				<Avatar
+					src={`http://localhost:8000/uploads/userAvatar/${user.avatar}`}
+					alt={user.username}/>
+			</IconButton>
+			<Menu
+				id="menu-appbar"
+				anchorEl={anchorEl}
+				anchorOrigin={{
+					vertical: 'top',
+					horizontal: 'right',
+				}}
+				keepMounted
+				transformOrigin={{
+					vertical: 'top',
+					horizontal: 'right',
+				}}
+				open={open}
+				onClose={handleClose}
+			>
+				<ListItem disabled>Привет, {user.username}!</ListItem>
+				<Divider/>
+				<MenuItem onClick={logout}>Выйти</MenuItem>
+			</Menu>
+		</>
+	);
+};
+
+export default UserMenu;
