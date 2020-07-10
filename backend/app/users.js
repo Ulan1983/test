9b9c88e3bcfdc0af2bb387355c06cdd338eb055a
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-07-10 19:12:29 +0600

    Добавил удаление пользователя

--- backend/app/users.js
+++ backend/app/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const auth = require('../middlewares/auth');
+const permit = require('../middlewares/permit');
 const upload = require('../multer');
 
 const User = require('../models/User');
@@ -13,7 +14,20 @@ router.get('/', auth, async (req, res) => {
 		const users = await User.find();
 		res.send(users)
 	} catch (error) {
-		return res.status(400).send(error);
+		return res.status(500).send(error);
+	}
+});
+
+router.get('/:id', auth, async (req, res) => {
+	try {
+		const user = await User.findOne({_id: req.params.id});
+
+		if (!user) {
+			return res.status(404).send({error: 'Не найдено'});
+		}
+		res.send(user);
+	} catch (error) {
+		return res.status(500).send(error);
 	}
 });
 
@@ -36,7 +50,7 @@ router.post('/', upload.single('avatar'), async (req, res) => {
 
 		return res.send(user._id);
 	} catch (error) {
-		res.status(400).send(error)
+		res.status(500).send(error)
 	}
 });
 
@@ -102,7 +116,25 @@ router.put('/', auth, upload.single('avatar'), async (req, res) => {
 		await user.save();
 		return res.send(user);
 	} catch (error) {
-		return res.status(400).send(error);
+		return res.status(500).send(error);
+	}
+});
+
+router.delete('/:id', [auth, permit('admin')], async (req, res) => {
+	try {
+		const user = await User.findOne({_id: req.params.id});
+
+		if (!user) {
+			return res.status(404).send({error: 'Не найдено'});
+		}
+
+		if (user._id.toString() === req.user._id.toString()) {
+			return res.status(401).send({error: 'Вы не можете удалить сами себя'});
+		}
+		await user.delete({_id: req.params.id});
+		return res.send({message: 'Deleted successfully'});
+	} catch (error) {
+		return res.status(500).send(error);
 	}
 });
 
