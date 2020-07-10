17def3cb9b86c2bd98083c843fde939c7ed6481d
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-07-10 19:16:47 +0600

    Добавил новые роуты

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -6,6 +6,8 @@ import {useSelector} from "react-redux";
 import MainPage from "./containers/MainPage/MainPage";
 import AppToolbar from "./components/UI/Toolbar/AppToolbar";
 import EditUser from "./containers/EditUser/EditUser";
+import UsersList from "./containers/UsersList/UsersList";
+import UserInfo from "./containers/UserInfo/UserInfo";
 
 
 const ProtectedRoute = ({isAllowed, ...props}) => (
@@ -24,6 +26,8 @@ const Routes = () => {
 				<Route path="/register" exact component={Register}/> />
 				<Route path="/login" exact component={Login}/> />
 				<ProtectedRoute isAllowed={user} path="/profile" exact component={EditUser}/>
+				<ProtectedRoute isAllowed={user} path="/usersList" exact component={UsersList}/>
+				<ProtectedRoute isAllowed={user} path="/user/:id" exact component={UserInfo}/>
 			</Switch>
 		</>
 	);
