9fa50cac81032c22f061b2e10bc2aae81926e287
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-07-10 19:17:24 +0600

    Добавил страницы со списком пользователей и просмотром одного пользователя

---
+++ frontend/src/containers/UserInfo/UserInfo.js
@@ -0,0 +1,89 @@
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {deleteUser, fetchUser} from "../../store/actions/usersActions";
+import Grid from "@material-ui/core/Grid";
+import {makeStyles} from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Modal from "../../components/UI/Modal/Modal";
+import Box from "@material-ui/core/Box";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles({
+	grid: {
+		margin: '0 auto'
+	},
+	root: {
+		minWidth: 275,
+	},
+	btn: {
+		marginLeft: '2%',
+		marginBottom: '2%'
+	}
+});
+
+const UserInfo = props => {
+	const singleUser = useSelector(state => state.users.singleUser);
+	const user = useSelector(state => state.users.user);
+	const error = useSelector(state => state.users.error);
+	const dispatch = useDispatch();
+	const classes = useStyles();
+	const [open, setOpen] = useState(false);
+	const handleClickOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	useEffect(() => {
+		dispatch(fetchUser(props.match.params.id));
+	}, [dispatch, props.match.params.id]);
+
+	const removeUser = async () => {
+		dispatch(deleteUser(props.match.params.id));
+	};
+
+	return (
+		<>
+			{singleUser &&
+				<Grid item xs={12} lg={9} sm={8} ml={8} className={classes.grid}>
+					<Card>
+						<CardContent>
+							<b>Никнейм: </b>{singleUser.username}
+						</CardContent>
+						{user && user.role === 'admin' &&
+							<Grid item>
+								<Button color="secondary" variant="outlined"
+										className={classes.btn} onClick={handleClickOpen}>
+									Удалить
+								</Button>
+							</Grid>
+						}
+					</Card>
+					<Modal onClose={handleClose} open={open} title="Вы действительно хотите удалить этого пользователя?">
+						<Grid container justify='center' spacing={1}>
+							<Grid item>
+								<Button
+									variant="contained"
+									color="primary"
+									onClick={handleClose}
+								>Нет</Button>
+							</Grid>
+							<Grid item>
+								<Button
+									variant="contained"
+									color="secondary"
+									onClick={removeUser}
+									id='yes'
+								>Да</Button>
+							</Grid>
+						</Grid>
+						{error && <Box mb={1}>
+							<Alert severity="error">{error.error}</Alert>
+						</Box>}
+					</Modal>
+				</Grid>
+			}
+		</>
+	);
+};
+
+export default UserInfo;
---
+++ frontend/src/containers/UsersList/UsersList.js
@@ -0,0 +1,57 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchUsers} from "../../store/actions/usersActions";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+import {NavLink} from "react-router-dom";
+
+const useStyles = makeStyles({
+	grid: {
+		margin: '0 auto',
+		padding: '10px'
+	},
+	root: {
+		minWidth: 275,
+	},
+});
+
+const UsersList = () => {
+	const users = useSelector(state => state.users.users);
+	const dispatch = useDispatch();
+	const classes = useStyles();
+
+	useEffect(() => {
+		dispatch(fetchUsers())
+	}, [dispatch]);
+
+	return (
+		<Grid item xs={12} sm={6} md={4} className={classes.grid}>
+			{users && users.map(user => (
+				<Card key={user._id} className={classes.root}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							<b>Никнейм: </b>{user.username}
+						</Typography>
+					</CardContent>
+					<CardActions>
+						<Button size="small"
+								component={NavLink} to={`/user/${user._id}`}
+								color="primary"
+								variant="contained"
+						>
+							Просмотр
+						</Button>
+					</CardActions>
+				</Card>
+			))
+			}
+		</Grid>
+	);
+};
+
+export default UsersList;
